5af89036e1766c9757b501afcc0dda4a492a37ec
albgalhue <albertogh1571@gmail.com>
2024-12-18 22:57:19 +0100

    Tipos de datos: Null value

---
+++ 2-Tipos de datos/2.7-Null.sh
@@ -0,0 +1,13 @@
+# 2.7  Null o vacío
+echo "\n2.7 Null value\n"
+
+# Bash utiliza cadenas vacías("") o variables no definidas(var) para representar un valor nulo o vacío.
+
+echo "EJEMPLOS:\n"
+echo "Comprobar si una variable está vacía:"
+
+var=""
+
+if [ -z "$var" ]; then
+    echo "La variable está vacía"
+fi
